f1547958c6d16ffb037f68cbe6b9ebdab74aac5e
Ankit Raj <60788484+Ankit75124@users.noreply.github.com>
2022-10-15 02:53:33 +0530

    self solution

---
+++ Array/MergeSortedArray/2nd.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>res;
+        for(int i=0;i<m;i++){
+            res.push_back(nums1[i]);
+            
+        }
+        for(int i=0;i<n;i++){
+            res.push_back(nums2[i]);
+            
+        }
+        
+        sort(res.begin(),res.end());
+        
+    nums1=res;
+    }
+};
