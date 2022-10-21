645038c43ca532446368fa0ab0b6e990f1f1cf6e
Ankit Raj <60788484+Ankit75124@users.noreply.github.com>
2022-10-21 23:37:32 +0530

    Solution

---
+++ Hash/219.Contains-Duplicate-II/219-solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+    for(int i = 0; i < nums.size(); i++){
+      if(map.count(nums[i]) && (i - map[nums[i]] <= k)) return true;
+      map[nums[i]] = i;
+    }
+    return false;
+    }
+};
