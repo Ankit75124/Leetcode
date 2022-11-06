1331b6f4ef4afe78e3f186b9844689337abca875
Ankit Raj <60788484+Ankit75124@users.noreply.github.com>
2022-11-06 22:52:14 +0530

    solution

---
+++ String/1768. Merge Strings Alternately/1768-solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+         int n = word1.size(), m = word2.size(), i = 0, j = 0;
+        string res = "";
+        while (i < n || j < m) {
+            if (i < n)
+                res.push_back(word1[i++]);
+            if (j < m)
+                res.push_back(word2[j++]);
+        }
+        return res;
+    }
+};
