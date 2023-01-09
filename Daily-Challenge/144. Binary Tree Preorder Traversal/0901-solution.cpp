520323a444f1d42d9ba6c492972c4d047c4d37c8
Ankit Raj <60788484+Ankit75124@users.noreply.github.com>
2023-01-09 21:34:20 +0530

    Create 0901-solution.cpp

---
+++ Daily-Challenge/144. Binary Tree Preorder Traversal/0901-solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        while (root) {
+            if (root -> left) {
+                TreeNode* pre = root -> left;
+                while (pre -> right && pre -> right != root) {
+                    pre = pre -> right;
+                }
+                if (!pre -> right) {
+                    pre -> right = root;
+                    nodes.push_back(root -> val);
+                    root = root -> left;
+                } else {
+                    pre -> right = NULL;
+                    root = root -> right;
+                }
+            } else {
+                nodes.push_back(root -> val);
+                root = root -> right;
+            }
+        }
+        return nodes;
+    }
+};
